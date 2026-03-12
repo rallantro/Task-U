c8323e2d860437c8298054a14238c9bb9896a193
rallantro <ronaldovliren@gmail.com>
2026-03-11 23:03:04 -0300

    Ajuste de Tarefas Semanal

--- Models/TarefaService.cs
+++ Models/TarefaService.cs
@@ -11,6 +11,53 @@ namespace Todo_Gacha.Models
     {
         private AppDbContext context;
 
+        public void AtualizarTarefas()
+        {
+            using var context = new AppDbContext();
+            var hoje = DateTime.Now.DayOfWeek;
+
+            context.Tarefas.RemoveRange(context.Tarefas);
+
+            if (hoje == DayOfWeek.Monday) 
+            {
+                context.Tarefas.Add(new Tarefa { Name = "Vídeo-Aula: PT/Mat", Desc = "30min de Português e 30min de Matemática", Dif = 3, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Exercícios: Português", Desc = "Bloco de exercícios de Linguagens", Dif = 4, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Estudo Pesado: Álgebra", Desc = "Álgebra e Funções (Foco total)", Dif = 5, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Revisão Noturna", Desc = "Revisão do conteúdo do dia", Dif = 2, IsDone = false });
+            }
+            else if (hoje == DayOfWeek.Tuesday) 
+            {
+                context.Tarefas.Add(new Tarefa { Name = "Vídeo-Aula: His/Fis", Desc = "30min de História e 30min de Física", Dif = 3, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Exercícios: História", Desc = "Prática de Humanas", Dif = 4, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Estudo Pesado: Física I", Desc = "Mecânica e Leis de Newton", Dif = 5, IsDone = false });
+            }
+            else if (hoje == DayOfWeek.Wednesday) 
+            {
+
+                context.Tarefas.Add(new Tarefa { Name = "Vídeo-Aula: Bio/Mat", Desc = "30min de Biologia e 30min de Matemática", Dif = 3, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Exercícios: Citologia/Eco", Desc = "Biologia celular e ecologia", Dif = 4, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Estudo Pesado: Geometria", Desc = "Geometria Plana/Espacial", Dif = 5, IsDone = false });
+            }
+            else if (hoje == DayOfWeek.Thursday) 
+            {
+                context.Tarefas.Add(new Tarefa { Name = "Vídeo-Aula: Geo/Qui", Desc = "30min de Geografia e 30min de Química", Dif = 3, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Exercícios: Geografia", Desc = "Prática de Geografia Geral/Brasil", Dif = 4, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Estudo Pesado: Física I", Desc = "Reforço em Mecânica", Dif = 5, IsDone = false });
+            }
+            else if (hoje == DayOfWeek.Friday) 
+            {
+                context.Tarefas.Add(new Tarefa { Name = "Redação: Criação", Desc = "Desenvolvimento de tema para vestibular", Dif = 6, IsDone = false }); // DIF 6 ativa LuckEvent!
+                context.Tarefas.Add(new Tarefa { Name = "Estudo Pesado: Física II", Desc = "Eletricidade e Ondas", Dif = 5, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Química/Biologia", Desc = "Bloco de Natureza vespertino", Dif = 4, IsDone = false });
+            }
+            else if (hoje == DayOfWeek.Saturday) 
+            {
+                context.Tarefas.Add(new Tarefa { Name = "SIMULADO GERAL", Desc = "Execução do simulado cronometrado", Dif = 10, IsDone = false }); // Muitos cristais aqui!
+                context.Tarefas.Add(new Tarefa { Name = "Revisão do Simulado", Desc = "Análise de erros e acertos", Dif = 5, IsDone = false });
+                context.Tarefas.Add(new Tarefa { Name = "Filosofia/Sociologia", Desc = "Leitura de Humanas", Dif = 3, IsDone = false });
+            }
+        }
+
         public void verStatus()
         {
             using var context = new AppDbContext();
@@ -24,7 +71,7 @@ namespace Todo_Gacha.Models
             foreach (Tarefa tarefa in context.Tarefas.Where(x => x.IsDone == false).ToList())
             {
                 Console.WriteLine("--");
-                Console.WriteLine($"Nome:{tarefa.Name} Descrição: {tarefa.Desc} Dificuldade: {tarefa.Dif}");
+                Console.WriteLine($"ID: {tarefa.Id} Nome:{tarefa.Name} Descrição: {tarefa.Desc} Dificuldade: {tarefa.Dif}");
             }
         }
         public void ConcluirTarefa(int TarefaId, Gacha gacha)
--- Models/User.cs
+++ Models/User.cs
@@ -11,5 +11,7 @@ namespace Todo_Gacha.Models
         public int Crystals { get; set; }
         public int PityLeg{ get; set; }
         public int PityEpic { get; set; }
+
+        public DateTime lastLogin {get; set;}
     }
 }
